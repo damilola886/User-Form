20ea3b5abefd9b3caa14aec70aecb218adfbc58c
unknown <damilolaoladimeji23@gmail.com>
2020-03-29 21:20:52 +0100

    A form to create and delete users with vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -18,7 +18,6 @@
 }
 
 #nav {
-  padding: 30px;
 }
 
 #nav a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h2>A Form to Create a User</h2>
+    <div class="row mt-5">
+      <div class="col-md-5 offset-md-1">
+        <form>
+          <div class="input-group mb-3">
+            <input
+              v-model="newUserName"
+              type="text"
+              placeholder="Enter name of User"
+              class="form-control"
+            />
+            <div class="input-group-append">
+              <button type="button" @click="saveUser" class="btn btn-secondary">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="col-md-6">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="user in users" :key="user.id">
+            {{ user.name }} <span @click="delUser(user)">X</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      users: [],
+      newUserName: null
+    };
+  },
+  async created() {
+    let collectUsers = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(collectUsers);
+    this.users = collectUsers.data;
+  },
+  methods: {
+    saveUser() {
+      if (!this.existingUser(this.newUserName)) {
+        let lastUserIndex = this.users.length - 1;
+        let lastUser = this.users[lastUserIndex];
+
+        let newUserid = lastUser.id + 1;
+
+        let newuser = {
+          id: newUserid,
+          name: this.newUserName
+        };
+
+        this.users.push(newuser);
+        this.newUserName = "";
+      }
+    },
+    existingUser(newUserName) {
+      let duplicate = false;
+
+      this.users.forEach(user => {
+        if (user.name === newUserName) {
+          duplicate = true;
+          alert(
+            "This User " + this.newUserName + " already exits in this Database"
+          );
+
+          this.newUserName = null;
+          this.users.pop(newuser);
+        }
+      });
+    },
+
+    delUser(user) {
+      this.users = this.users.filter(person => person.id !== user.id);
+      console.log(user.id);
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+span {
+  color: red;
+  float: right;
+  cursor: pointer;
+}
+h2 {
+  background-color: #808080;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: black;
+  font-size: 40px;
+}
+</style>
